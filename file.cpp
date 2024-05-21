95e23e1dcc5dd9cfbea64cab65fa851e229b3004
MonoIit <maksim-smirnov-2004@inbox.ru>
2024-05-21 15:26:07 +0300

    only_GUI

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include "file.h"
+#include "ui_file.h"
+#include <QPushButton>
+
+File::File(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::File)
+{
+    ui->setupUi(this);
+    connect(ui->buttonBox->button(QDialogButtonBox::Ok), SIGNAL(clicked()), SLOT(okClicked()));
+    connect(ui->buttonBox->button(QDialogButtonBox::Cancel), SIGNAL(clicked()), SLOT(close()));
+}
+
+File::~File()
+{
+    delete ui;
+}
